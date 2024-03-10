655b02fbd9d8e07ca94001413e27c54e41381274
Babar Khan <52751757+bobiiii@users.noreply.github.com>
2024-03-10 15:12:58 +0500

    #

--- src/Screens/Our_Collection/Components/DiscoverCollection.js
+++ src/Screens/Our_Collection/Components/DiscoverCollection.js
@@ -512,33 +512,40 @@ const DiscoverCollection = () => {
   const collectionJSX = (v, i, islable = false, inner = false) => {
     return (
       <div
-        className="discover-collection "
-        style={{ margin: inner ? "20px 10px" : "" }}
+        className="  flex-grow flex-shrink basis-2/5      text-center  "
+        
       >
+        <div className="flex flex-col   ">
         <div
           onMouseOver={() => setOnMouseOver(i)}
           
-          className="discover-collectionimage "
-          style={{
-            backgroundImage: `url(${v?.color_image})`,
-          }}
+          className=" place-self-center  "
+          
         >
+          <div className="relative">
+          <img src={`${v?.color_image}`} className="h-40 w-40 rounded-lg  "/>
           {i === onMouseOver && (
             <div
               onClick={() => gotoNextScreen(v)}
-              className="discover-collectionexpand "
+              className=" bg-black text-white w-min  px-4 py-4 rounded-full absolute  top-[35%] left-[50%]  translate-x-[-50%] "
             >
               <GoArrowUpRight size={35} color="white" />
             </div>
           )}
+          </div>
         </div>
+
+
         {islable ? (
-          <div className="discover-collectionlabel">
-            <div className="discover-collectionlabel1">{v.color_name}</div>
+          <div className="text-center ">
+            <div className="text-center">{v.color_name}</div>
           </div>
         ) : (
-          <div className="discover-collectiontext">{v.color_name}</div>
+          <div className="text-center text-wrap w-3/5  mx-auto pt-1   ">{v.color_name}</div>
         )}
+        
+</div>
+      
       </div>
     );
   };
@@ -579,16 +586,25 @@ const DiscoverCollection = () => {
   }, []);
   return (
     <MaxWidthWrapper>
-      <div className="discover-collection-component">
-        <div className="discover-collection-heading">
+      <div className="py-10">
+        <div className="font-extrabold text-3xl text-center font-gelasio">
           {flag ? content[collection][0] : innerCollection[0]?.collection_name}
         </div>
-        <div className="discover-collection-desc">{content[collection][1]}</div>
-        {/* discover-collection-collections */}
-        {/* w-full flex justify-center items-center flex-wrap */}
-        {/* <div className="w-full flex justify-center items-center mx-auto"> */}
-          {/* flex-grow gap-10  grid  justify-center items-center  sm:grid-cols-3 grid-cols-2 */}
-          <div className="w-full flex justify-center items-center flex-wrap mx-auto ">
+        <div className="text-center py-4">{content[collection][1]}</div>
+        
+{/* FOR Dynamic Data */}
+{/* <div className=" flex gap-4 justify-center  flex-wrap    ">
+            {flag
+              ? collections.length > 0
+                ? <><div className=" border border-green-600 flex-grow flex-shrink basis-2/5      text-center  ">a</div> {collections.map((v, i) => collectionJSX(v, i))}</>
+                : loader()
+              : innerCollection.length > 0
+                ? innerCollection?.map((v, i) => collectionJSX(v, i, true, true))
+                : loader()}
+          </div> */}
+
+
+          <div className=" flex gap-4 justify-center  flex-wrap    ">
             {flag
               ? collections.length > 0
                 ? collections.map((v, i) => collectionJSX(v, i))
