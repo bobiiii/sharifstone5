f403dba2c89da17f8e5d15268732cf4c90580208
Babar Khan <52751757+bobiiii@users.noreply.github.com>
2024-03-14 23:02:07 +0500

    #

--- src/components/coverComponent.js
+++ src/components/coverComponent.js
@@ -5,7 +5,7 @@ const CoverComponent = ({image, label}) => {
     return(
         <div className='coverComponent overflow-hidden' >
         
-        <div className='bg-red-300 w-full h-full hover:scale-110 transition-all duration-700 grid place-items-center bg-no-repeat bg-cover'  style={{ backgroundImage: image}}>
+        <div className=' w-full h-full hover:scale-110 transition-all duration-700 grid place-items-center bg-no-repeat bg-cover'  style={{ backgroundImage: image}}>
         {label[0]}
         <br />{label[1]}
 
