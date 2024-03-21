66f1a4b0294c1c3882be0c9ab0d7e89dc40b15dd
Babar Khan <52751757+bobiiii@users.noreply.github.com>
2024-03-21 18:19:09 +0500

    #

--- src/components/coverComponent.js
+++ src/components/coverComponent.js
@@ -30,12 +30,12 @@ const CoverComponent = ({ imageMobile,imageDesktop,  label }) => {
 
   return (
     <div className='coverComponent overflow-hidden ' onMouseOver={() => { setShowDropdown(false) }}>
-      <div className='w-full brg  2xl:h-[60vh]  h-[530px] grid place-items-center ' >
-        <div className='brg relative border-4 border-green-600  h-full w-screen'>
-          <img src={`${imageMobile}`} alt='cover' className='lg:hidden bg-no-repeat bg-center bg-cover  h-full w-full' />
-          <img src={`${imageDesktop}`} alt='cover' className='hidden lg:block bg-no-repeat bg-center bg-cover  h-full w-full' />
+      <div className='w-full brg  2xl:h-[80vh]  h-[530px] grid place-items-center ' >
+        <div className='brg relative border-4  h-full w-screen'>
+          <img src={`${imageMobile}`} alt='cover' className='lg:hidden bg-no-repeat bg-center bg-cover  h-full w-full  zoomedImage' />
+          <img src={`${imageDesktop}`} alt='cover' className='hidden lg:block bg-no-repeat bg-center bg-cover  h-full w-full  zoomedImage' />
 
-          <div className='brg absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center '>
+          <div className=' absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center '>
             {label[0]}<br />{label[1]}
           </div>
         </div>
