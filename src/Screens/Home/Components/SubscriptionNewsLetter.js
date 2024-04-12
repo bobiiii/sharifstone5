62a46fb9079ab0e8345fed4d22af668cd06278db
Usmanzaib356 <110809791+Usmanzaib356@users.noreply.github.com>
2024-04-12 04:11:44 -0700

    added validation

--- src/Screens/Home/Components/SubscriptionNewsLetter.js
+++ src/Screens/Home/Components/SubscriptionNewsLetter.js
@@ -1,22 +1,34 @@
-/* eslint-disable react/react-in-jsx-scope */
 import "./SubscriptionNewsLetter.css";
 import BG from "../../../assets/images/homeImage/subscript_bg.png";
 import { IoIosSend } from "react-icons/io";
 import { useState } from "react";
 import MaxWidthWrapper from "../../MaxWidthWrapper";
 import Heading from "../../resuable/Heading";
-import {addNewsletter} from "../../../apiCall/apiCall";
+import { addNewsletter } from "../../../apiCall/apiCall";
 
 const SubscriptionNewsLetter = () => {
   const [emailAdress, setEmailAdress] = useState("");
+  const [emailAdressMessage, setEmailAdressMessage] = useState("");
   const [emailSuccess, setEmailSuccess] = useState(false);
-  const submitEmail = async(e) => {
+
+  const submitEmail = async (e) => {
     e.preventDefault();
-    const res = await addNewsletter(emailAdress)
-    if (res.status == 200) {
-      setEmailSuccess(true)
+    if (emailAdress.trim() === "") {
+      setEmailAdressMessage("Email address cannot be empty");
+      setTimeout(() => {
+        setEmailAdressMessage("");
+      }, 2000);
+      return
     }
-  }
+
+    const res = await addNewsletter(emailAdress);
+    if (res.status === 200) {
+      setEmailSuccess(true);
+    }
+  };
+
+
+
   return (
     <MaxWidthWrapper className='px-0'>
       <div className="w-full">
@@ -25,7 +37,7 @@ const SubscriptionNewsLetter = () => {
             SUBSCRIBE TO OUR NEWSLETTER TO GET UPDATES ON NEW PROMOTIONS AND
             PRODUCTS
           </Heading>
-          <div className=" lg:w-2/3 md:w-[70%] sm:w-[90%] w-full flex justify-center items-center ">
+          <div className=" lg:w-2/3 md:w-[70%] sm:w-[90%] w-full flex flex-col justify-center items-center ">
             <form style={{ marginBottom: 30 }} className="lg:w-[70%] md:w-[80%]
              w-full sm:px-2 px-4 flex " >
               <input
@@ -36,15 +48,16 @@ const SubscriptionNewsLetter = () => {
                 type="text"
                 required
               />
-              <button  onClick={(e)=>{submitEmail(e)}} className=" bg-[#ee2a2e]  text-center items-center px-10 py-2 -ml-6 rounded-[30px] md:flex hidden whitespace-nowrap text-white font-semibold sm:text-base text-sm  font-gelasio z-30">
-                Subcribe
+              <button onClick={(e) => { submitEmail(e) }} className=" bg-[#ee2a2e]  text-center items-center px-10 py-2 -ml-6 rounded-[30px] md:flex hidden whitespace-nowrap text-white font-semibold sm:text-base text-sm  font-gelasio z-30">
+                Subscribe
               </button >
-              <button  onClick={(e)=>{submitEmail(e)}} className=" bg-[#ee2a2e] z-30 text-center items-center px-6 py-2.5 -ml-6 rounded-[30px] whitespace-nowrap md:hidden flex text-white font-semibold  font-gelasio">
+              <button onClick={(e) => { submitEmail(e) }} className=" bg-[#ee2a2e] z-30 text-center items-center px-6 py-2.5 -ml-6 rounded-[30px] whitespace-nowrap md:hidden flex text-white font-semibold  font-gelasio">
                 <IoIosSend size={25} color={"white"} />
               </button>
             </form>
+            {emailAdressMessage && <p className="text-red-500">{emailAdressMessage}</p>}
+            {emailSuccess && <h3 className="text-red-500 text-center text-lg lg:text-3xl font-bold">Thank You For Being a Part of SharifStone! </h3>}
           </div>
-         {emailSuccess && <h3 className="text-red-500 text-center text-lg lg:text-3xl font-bold">Thank You For Being a Part of SharifStone! </h3>}
           <img className="home-subscrion-bg" src={BG} />
         </div>
       </div>
